4bc31ac94d6ba5f4a04b6b71e8393681f200c288
Agustí Moll <agusti.moll@guifi.net>
2015-07-11 11:38:35 +0200

    Remove libopencv-highgui change to libopencv-videoio

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ ARCH=
 
 INCLUDE_FLAGS=-I/usr/local/include
 CFLAGS=-c -Wall -Wno-write-strings $(INCLUDE_FLAGS) 
-LDFLAGS= -L/usr/local/lib -lopencv_core -lopencv_highgui -lopencv_imgproc -lzbar
+LDFLAGS= -L/usr/local/lib -lopencv_core -lopencv_imgproc -lopencv_videoio -lzbar
 
 SOURCES = logger.cpp main.cpp
 OBJECTS=$(SOURCES:.cpp=.o)
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/videoio/videoio.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <zbar.h>
 #include <iostream>
@@ -26,15 +26,8 @@ int main(int argc, char* argv[])
     }
 
 
-    ImageScanner scanner;  
-      scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);  
-
-    if (DEBUG == 1) {
-      double dWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH); //get the width of frames of the video
-      double dHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT); //get the height of frames of the video
-      log_print(INFO_LOG,"Frame size: %d x %d", dWidth,dHeight);
-      namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-    }
+    ImageScanner scanner;
+      scanner.set_config(ZBAR_NONE, ZBAR_CFG_ENABLE, 1);
 
     while (1)
     {
@@ -51,44 +44,36 @@ int main(int argc, char* argv[])
         Mat grey;
         cvtColor(frame,grey,CV_BGR2GRAY);
 
-        int width = frame.cols;  
-        int height = frame.rows;  
-        uchar *raw = (uchar *)grey.data;  
-        // wrap image data  
-        Image image(width, height, "Y800", raw, width * height);  
-        // scan the image for barcodes  
+        int width = frame.cols;
+        int height = frame.rows;
+        uchar *raw = (uchar *)grey.data;
+        // wrap image data
+        Image image(width, height, "Y800", raw, width * height);
+        // scan the image for barcodes
         scanner.scan(image);
 
-        // extract results  
-        for(Image::SymbolIterator symbol = image.symbol_begin();  symbol != image.symbol_end();  ++symbol) {  
-          vector<Point> vp;  
+        // extract results
+        for(Image::SymbolIterator symbol = image.symbol_begin();  symbol != image.symbol_end();  ++symbol) {
+          vector<Point> vp;
           // do something useful with results
-          //cout << symbol->get_data() << endl;  
+          //cout << symbol->get_data() << endl;
           printf("%s\n", symbol->get_data().c_str());
           //log_print(INFO_LOG, "%s", symbol->get_data().c_str());
           return 0;
-           
-          int n = symbol->get_location_size();  
-          for(int i=0;i<n;i++){  
-                vp.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i))); 
-          }  
-          RotatedRect r = minAreaRect(vp);  
-          Point2f pts[4];  
-          r.points(pts);  
-          for(int i=0;i<4;i++){  
-            line(frame,pts[i],pts[(i+1)%4],Scalar(255,0,0),3);  
+
+          int n = symbol->get_location_size();
+          for(int i=0;i<n;i++){
+                vp.push_back(Point(symbol->get_location_x(i),symbol->get_location_y(i)));
           }
-          log_print(INFO_LOG,"Angle: %d", r.angle);  
-        }  
+          RotatedRect r = minAreaRect(vp);
+          Point2f pts[4];
+          r.points(pts);
+          for(int i=0;i<4;i++){
+            line(frame,pts[i],pts[(i+1)%4],Scalar(255,0,0),3);
+          }
+          log_print(INFO_LOG,"Angle: %d", r.angle);
+        }
 
-        if (DEBUG == 1) 
-          imshow("MyVideo", frame); //show the frame in "MyVideo" window
-
-        if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-        {
-          log_print(INFO_LOG,"ESC key is pressed by user");
-          break; 
-       }
     }
     return 0;
 
